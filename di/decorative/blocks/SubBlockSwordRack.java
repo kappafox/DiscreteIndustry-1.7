100b02d93a48830e2c54d3e36017dcec473fe711
kappafox <kappa.fox@gmail.com>
2014-09-20 15:05:08 +1000

    * Fix incorrect texture names

--- di/decorative/blocks/SubBlockSwordRack.java
+++ di/decorative/blocks/SubBlockSwordRack.java
@@ -33,8 +33,8 @@ public class SubBlockSwordRack extends SubBlock
 	@Override
 	public void registerBlockIcons(IIconRegister ireg) 
 	{
-		side = ireg.registerIcon(DiscreteIndustry.MODID + ":" + "blockSwordRackside");
-		top = ireg.registerIcon(DiscreteIndustry.MODID + ":" + "blockSwordRacktop");
+		side = ireg.registerIcon(DiscreteIndustry.MODID + ":" + "blockSwordRack_side");
+		top = ireg.registerIcon(DiscreteIndustry.MODID + ":" + "blockSwordRack_top");
 
 	}
 
